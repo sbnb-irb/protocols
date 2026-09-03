c9bd0ccef49d94c84328b3ce9d4085b6d9886bd1
sayalaruano <s_ayalaruano@proton.me>
2026-09-03 14:29:50 +0200

    💚 Build: update the sphinx build and reathedocs scripts to copy notebooks from orifinal root location

--- docs/sphinx_build.sh
+++ docs/sphinx_build.sh
@@ -1,6 +1,7 @@
 export SPHINX_APIDOC_OPTIONS="members,undoc-members,inherited-members,show-inheritance"
 
-jupytext notebooks/*.ipynb --sync
+mkdir -p notebooks
+cp ../notebooks/*.ipynb notebooks/
 
 # Generating the rst files for the API reference. This is needed for autodoc during sphinx-build to work.
 # Note: does not use conf.py
